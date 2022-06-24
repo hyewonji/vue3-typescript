8f1b5b873d5afaf1801178b9710c9e16588269f5
hyewonji <wlgpdnjs8873@naver.com>
2022-06-24 16:30:24 +0900

    Set login router base

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,19 +1,23 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
-import HomeView from '../views/HomeView.vue';
+import HomeView from '@/pages/HomeView.vue';
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
-    name: 'home',
-    component: HomeView,
+    redirect: 'login',
   },
   {
-    path: '/about',
-    name: 'about',
+    path: '/login',
+    name: 'login',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue'),
+    component: () => import('@/pages/LoginView.vue'),
+  },
+  {
+    path: '/home',
+    name: 'home',
+    component: HomeView,
   },
 ];
 
