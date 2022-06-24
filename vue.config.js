4b0796cb79f439c4b2f6791a538076552b1708f3
hyewonji <wlgpdnjs8873@naver.com>
2022-06-24 16:33:53 +0900

    set import path alias to @

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,13 @@
 const { defineConfig } = require('@vue/cli-service');
+const path = require('path');
 
 module.exports = defineConfig({
+  configureWebpack: {
+    resolve: {
+        alias: {
+          '@': path.join(__dirname, 'src/')
+        }
+    }
+  },
   transpileDependencies: true,
 });
